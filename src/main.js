6e9f0dba064dcdd868b538b755bf28666b89f884
Harrison Shoebridge <harrison@theshoebridges.com>
2018-03-02 23:04:18 +1100

    Add some slightly more sensical cell physics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-var cells = [];
-var app;
+let cells;
+let cellSize = 20;
+let blobX = 200;
+let blobY = 200;
+let app;
 
 function init() {
   let canvas = document.getElementById('game');
@@ -16,45 +19,83 @@ function init() {
 
   app.ticker.add(delta => update(delta));
 
-  let cellCount = 10;
+  cells = [];
+  let cellCount = 4;
   let cellCutOff = 9;
-  let cellSize = 20;
+
+  let ID = 0;
 
   for (let x = -cellCount/2; x < cellCount/2; x++) {
     for (let y = -cellCount/2; y < cellCount/2; y++) {
       if (x*x + y*y > cellCutOff) continue;
 
       let cell = new PIXI.Graphics();
+      cell._id = ID;
       cell.beginFill(0xFFFFFF);
       cell.drawEllipse(0, 0, cellSize, cellSize);
       cell.endFill();
 
-      cell.hsOx = cell.x = 200 + (cellSize + cellSize/2)*x;
-      cell.hsOy = cell.y = 200 + (cellSize + cellSize/2)*y;
-
-      cell.hsOffset = (0.3 + Math.random()/10 * 7) * 2;
-      cell.hsOffset2 = Math.random();
-      cell.hsDir = (Math.random() > 0.5) ? -1 : 1;
+      cell.x = blobX + (cellSize) * x;
+      cell.y = blobY + (cellSize) * y;
 
       app.stage.addChild(cell);
       cells.push(cell);
+
+      ID++;
     }
   }
 }
 
 function update(dt) {
-  let pulseSpeed = 2;
-  for (let cell of cells) {
-    let d = 1 + Math.cos(app.ticker.lastTime/1000 * cell.hsOffset * pulseSpeed)*0.05;
-    cell.scale.x = d;
-    cell.scale.y = d;
+  for (let x of cells) {
+    let vx = x.x - blobX;
+    let vy = x.y - blobY;
+    let len = Math.sqrt(vx*vx + vy*vy); //calculating length
 
-    d = 0.9 + Math.sin(app.ticker.lastTime/1000 * cell.hsOffset) * 0.1;
+    if (isNaN(len)) continue;
 
-    cell.alpha = d;
+    vx /= len;
+    vy /= len;
 
-    cell.x = cell.hsOx + Math.sin(app.ticker.lastTime/1000 * cell.hsOffset * cell.hsDir) * 3 * cell.hsOffset2;
-    cell.y = cell.hsOy + Math.cos(app.ticker.lastTime/1000 * cell.hsOffset * cell.hsDir) * 3 * cell.hsOffset2;
+    x.x -= vx * dt / 10;// * (len/10000);
+    x.y -= vy * dt / 10;// * (len/10000);
+
+    x.x += Math.cos(app.ticker.lastTime/1000 * Math.random()) * Math.random();
+    x.y += Math.sin(app.ticker.lastTime/1000 * Math.random()) * Math.random();
+  }
+
+  for (let a of cells) {
+    // find cells with which `cell` is overlapping with
+    // calculate the vector p1 - p2, normalise it
+    // and then move the cell in that direction;
+    // calculate unit vector of vx and vy
+
+    for (let b of cells) {
+      if (b._id == a._id) {
+        continue;
+      }
+
+      // find distance
+      let d = Math.sqrt(Math.pow(a.x-b.x, 2) + Math.pow(a.y-b.y, 2));
+
+      // check if intersecting
+      if (d > cellSize*1.3) {
+        continue;
+      }
+
+      let vx = a.x - b.x;
+      let vy = a.y - b.y;
+
+      // calculate unit vector of vx and vy
+      let len = Math.sqrt(vx*vx + vy*vy); //calculating length
+      if (isNaN(len)) continue;
+
+      vx /= len;
+      vy /= len;
+
+      a.x += vx * dt;
+      a.y += vy * dt;
+    }
   }
 }
 
