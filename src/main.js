81268d6bded489fbb67e26b7b911db3c878f2126
Harrison Shoebridge <harrison@theshoebridges.com>
2018-03-09 12:46:13 +1100

    Make lines spit groups

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+let blobs;
 let cells;
 let cellSize = 15;
 let blobX = 200;
 let blobY = 200;
+
+let pointOne;
+let pointTwo;
+
 let app;
 
 function init() {
@@ -19,9 +24,11 @@ function init() {
 
   app.stage.interactive = true;
   app.stage.hitArea = new PIXI.Rectangle(0, 0, 1000, 1000);
-  app.stage.on('pointerdown', spawnPoison);
+  app.stage.on('pointerdown', splitCells);
 
   app.ticker.add(delta => update(delta/1000));
+
+  blobs = [new Point(100, 200), new Point(300, 200)];
 
   cells = [];
   let cellCount = 6;
@@ -33,6 +40,12 @@ function init() {
       cell.x += 20 * (Math.random()*2 - 1);
       cell.y += 20 * (Math.random()*2 - 1);
 
+      if (Math.random() > 0.5) {
+        cell.blob = 0;
+      } else {
+        cell.blob = 1;
+      }
+
       cells.push(cell);
 
       app.stage.addChild(cell.g);
@@ -40,9 +53,45 @@ function init() {
   }
 }
 
-function spawnPoison(ev) {
-  console.log(ev);
+function splitCells(ev) {
+  if (!pointOne) {
+    pointOne = new Point(ev.data.global.x, ev.data.global.y);
+    return;
+  } else if (!pointTwo) {
+    pointTwo = new Point(ev.data.global.x, ev.data.global.y);
+  }
 
+  let g = new PIXI.Graphics();
+  g.lineStyle(2, 0xFF00FF);
+  g.moveTo(pointOne.x, pointOne.y);
+  g.lineTo(pointTwo.x, pointTwo.y);
+
+  app.stage.addChild(g);
+
+  for (let c of cells) {
+    let x = c.x;
+    let y = c.y;
+    let x1 = pointOne.x;
+    let y1 = pointOne.y;
+    let x2 = pointTwo.x;
+    let y2 = pointTwo.y;
+
+    let d = (x - x1)*(y2 - y1) - (y - y1)*(x2 - x1);
+
+    if (d > 0) {
+      c.blob = 0;
+      c.g.tint = 0xff00ff;
+    } else {
+      c.blob = 1;
+      c.g.tint = 0x00ff00;
+    }
+  }
+
+  pointOne = null;
+  pointTwo = null;
+}
+
+function spawnPoison(ev) {
   let cell = new Cell(ev.data.global.x, ev.data.global.y);
   cell.poison = 0.9;
 
@@ -57,9 +106,9 @@ function update(dt) {
 
     a.poison = Math.max(a.poison, 0);
 
-    // a.poison = Math.max(a.poison - 0.6 * dt, 0);
+    let center = blobs[c.blob];
 
-    let centerPull = new Point(c.x - blobX, c.y - blobY).unit();
+    let centerPull = new Point(c.x - center.x, c.y - center.y).unit();
     if (centerPull !== null) {
       a.velocity.x -= centerPull.x*7;
       a.velocity.y -= centerPull.y*7;
@@ -119,11 +168,28 @@ class Point {
   }
 }
 
-let ID = 0;
+class CellBlob {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
 
+    this.velocity = new Point(0, 0);
+    this.drag = 0.97; 
+  }
+
+  tick(dt) {
+    this.velocity.x *= this.drag;
+    this.velocity.y *= this.drag;
+
+    this.x += this.velocity.x * dt;
+    this.y += this.velocity.y * dt;
+  }
+}
+
+let Cell_ID = 0;
 class Cell {
   constructor(x, y) {
-    this._id = ID++;
+    this._id = Cell_ID++;
 
     this.g = new PIXI.Graphics();
 
@@ -158,11 +224,9 @@ class Cell {
 
   tick(dt) {
     if (this.poison > 0) {
-      // console.log(this.poison);
+      // lmao this is so dumb
       this.g.tint = eval("0x" + tinycolor("white").darken(this.poison * 100).toHex());
-    // this.g.tint = 0x000000;
     }
-
 
     this.velocity.x *= this.drag;
     this.velocity.y *= this.drag;
